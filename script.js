10a2f77cb5af92b117208e0bae5fe5b5f7fe6938
jexploit-x <pratamaaditya774@gmail.com>
2025-04-30 07:56:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function startDanaPayment() {
     // Simulate payment logic for DANA
     alert("Redirecting to DANA payment...");
     // You can replace this with actual payment logic
-    window.location.href = "https://link-to-dana-payment"; // Change to actual payment URL
+    window.location.href = "https://jexploit-x.github.io/Web-payment/paydana.html"; // Change to actual payment URL
 }
 
 function startQRISPayment() {
