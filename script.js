7c99a4b43886b1ab6f401871175375075cb853f5
jexploit-x <pratamaaditya774@gmail.com>
2025-05-02 09:58:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-function startDanaPayment() {
-    // Simulate payment logic for DANA
-    alert("Redirecting to DANA payment...");
-    // You can replace this with actual payment logic
-    window.location.href = "https://jexploit-x.github.io/Web-payment/paydana.html"; // Change to actual payment URL
-}
-
 function startQRISPayment() {
     // Simulate payment logic for QRIS
     alert("Redirecting to QRIS payment...");
     // You can replace this with actual payment logic
-    window.location.href = "https://link-to-qris-payment"; // Change to actual QRIS payment URL
+    window.location.href = "payqris.html"; // Change to actual QRIS payment URL
+}
+
+function startDanaPayment() {
+    // Simulate payment logic for DANA
+    alert("Redirecting to DANA payment...");
+    // You can replace this with actual payment logic
+    window.location.href = "paydana.html"; // Change to actual payment URL
+}
+
+function startGOPAYPayment() {
+    // Simulate payment logic for GOPAY
+    alert("Redirecting to GOPAY payment...");
+    // You can replace this with actual payment logic
+    window.location.href = "paygopay.html"; // Change to actual GOPAY payment URL
 }
